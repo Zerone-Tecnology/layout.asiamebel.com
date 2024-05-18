f2371dad84c53936fb72389bf4d20f01d00c8c01
Alisher <alisher@gmail.com>
2024-05-18 19:07:43 +0600

    popup

--- js/indira.js
+++ js/indira.js
@@ -5,6 +5,12 @@ $('.btn-popup-close').on('click', function(){
     $('#popupDetail').removeClass('active');
 })
 
+$('#btn-reviews').on('click', function(){
+    $('#popupReviews').addClass('active');
+})
+$('.btn-popup-close').on('click', function(){
+    $('#popupReviews').removeClass('active');
+})
 
 function Selected(a) {
     var label = a.value;
